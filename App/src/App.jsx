f560598d4485e0011eaaff8d2b68cca0e6d0606c
Samarth <samarth24558@gmail.com>
2024-08-21 00:11:37 -0700

    Added files

--- App/src/App.jsx
+++ App/src/App.jsx
@@ -2,11 +2,30 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  let [count, setCount] = useState(1)
+
+  function increment()
+  {
+    setCount(count+1)
+  }
+  function decrement()
+  {
+    setCount(count-1)
+         
+  }
+  function reset()
+  {
+    setCount(count=0)
+  }
 
   return (
     <>
-    <h1>Hello Nalanda</h1>
+    <h1>Counter App</h1>
+    <h4>{count}</h4>
+    <button onClick={increment}>Increment</button>
+    <button onClick={decrement}>Decrement</button>
+    <button onClick={reset}>Reset</button>
+
 
     </>
   )
